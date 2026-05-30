32b7fa9bdc146c0ee4d6ecb8d267e00157da2902
DipenShrestha7 <sdipen561@gmail.com>
2026-05-30 21:13:39 +0545

    Add fastify dependency to backend

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_URL ||
-    "postgresql://neondb_owner:npg_XvNtc6Df5GSq@ep-plain-cake-aoqe1xw2-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
-  {
-    dialect: "postgres",
-  },
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+});
 
 export default sequelize;
